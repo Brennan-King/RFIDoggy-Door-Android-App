ca1cf91d64f23b68073587bdbf639b7e0a5a6522
Brennan <bmkk85@umsystem.edu>
2019-09-12 10:51:03 -0500

    Added new activities for each button

---
+++ app/src/main/java/com/example/rfidoggy_door_android_app/ForceLockUnlock.java
@@ -0,0 +1,14 @@
+package com.example.rfidoggy_door_android_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForceLockUnlock extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_force_lock_unlock);
+    }
+}
---
+++ app/src/main/java/com/example/rfidoggy_door_android_app/SetTemperature.java
@@ -0,0 +1,14 @@
+package com.example.rfidoggy_door_android_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SetTemperature extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_temperature);
+    }
+}
